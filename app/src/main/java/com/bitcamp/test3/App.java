13113247d10403f223f2f67e31d19777e96f6bd1
jinseon0328 <wishofsun@daum.net>
2021-05-11 10:50:12 +0900

    .

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -4,20 +4,11 @@ public class App {
   public String getGreeting() {
     return "Hello Java!";
   }
-<<<<<<< HEAD
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-
-    for (int i = 0; i < 5; i++) {
-      System.out.println(i);
-
-    }
-    System.out.println("오늘 날씨가 좋네요!");
-=======
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
     System.out.println("점심시간에 잠깐 회의할까요");
+    System.out.println("점심시간에요...?");
 
     for (int i = 0; i < 5; i++) {
 
@@ -29,6 +20,5 @@ public class App {
         System.out.println(i);
       }
     }
->>>>>>> ef847f4b34d770895e0fe5c25caa2c7861d69e8f
   }
 }
