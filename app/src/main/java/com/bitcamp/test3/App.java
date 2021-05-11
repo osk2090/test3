3fa275fa63497539d81c014e1d9fa9244f20d14b
zeroback70 <zeroback70@naver.com>
2021-05-11 10:43:48 +0900

    d

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -4,11 +4,12 @@
 package com.bitcamp.test3;
 
 public class App {
-    public String getGreeting() {
-        return "Hello Java!";
-    }
+  public String getGreeting() {
+    return "Hello Java!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+    System.out.println("경도현");
+  }
 }
