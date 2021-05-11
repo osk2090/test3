cd99f0528d9a79d99132a4197392b0f525db9711
jinseon0328 <wishofsun@daum.net>
2021-05-11 11:44:42 +0900

    .

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -11,27 +11,9 @@ public class App {
     System.out.println("점심시간에 잠깐 회의할까요");
 
     for (int i = 0; i < 5; i++) {
-    System.out.println("햄버거먹으러가실분 ");
-    System.out.println("맛있는햄버거");
-    System.
-    System.out.println("배고프다");
-    System.out.
-    System.out.println("1
-    System.out.println("햄버거먹으러가실분 ");
-    System.out.println("맛있는햄버거");
-    System.out.println("배고프다22222222www22");
-    System.out.println("햄버거먹으러가실분222 ");
-    System.out.println("햄버거먹으러가실분 ");
-    System.out.println("맛있는햄버거");
-    System.out.println("배고프다22222222www22");
-    System.out.println("햄버거먹으러가실분222 ");
-111sssssss11111111");
-println("배고프다2222");
-.println("하이 HI~");
-
-n("배고프다2222");
-intln("배고프다22222222www22");
-    System.out.println("햄버거먹으러가실분222 ");
+      System.out.println("햄버거먹으러가실분 ");
+      System.out.println("맛있는햄버거");
+      System.out.println("에러 나는 거 푸쉬해서 죄송합니다");
 
 
       System.out.println("햄버거먹으러가실분 ");
