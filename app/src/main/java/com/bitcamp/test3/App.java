c8dce31945094e084d0fa7c862340b03af251022
jinseon0328 <wishofsun@daum.net>
2021-05-11 11:27:17 +0900

    sdd

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -5,22 +5,16 @@ public class App {
     return "Hello Java!";
   }
 
-<<<<<<< HEAD
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
     System.out.println("이클립스로 푸쉬!");
-=======
-
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
->>>>>>> refs/remotes/origin/main
     System.out.println("점심시간에 잠깐 회의할까요");
     System.out.println("점심시간에요...?");
     System.out.println("경도현");
+    System.out.println("zzzzzz");
 
-    System.out.println("햄버거먹으러가실분 ");
+    for (int i = 0; i < 5; i++) {
 
-<<<<<<< HEAD
       System.out.println(new App().getGreeting());
       System.out.println("ddddddddddddddddddaddddddddddddddd");
       System.out.println(new App().getGreeting());
@@ -29,8 +23,5 @@ public class App {
         System.out.println(i);
       }
     }
-=======
-    System.out.println("배고프다");
->>>>>>> refs/remotes/origin/main
   }
 }
